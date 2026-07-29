6fa8a5bc7f2eb9c8fcad7299178d0bc2e05dd5b1
Ana Luíza Dos Santos Souza <92101096+AnaLuizaS4ntos@users.noreply.github.com>
2026-07-29 18:10:00 -0300

    mexendo no flask

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+
 import sqlite3 
 import os 
 
@@ -16,32 +16,13 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'chave-secreta-unidesape
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.register_blueprint(products_bp, url_prefix='/api/products')
+
 app.register_blueprint(auth_bp, url_prefix='/api/auth')
-
-
-db = SQLAlchemy(auth_bp)
-
-# ### Login ###
-login_manager = LoginManager()
-login_manager.init_app(auth_bp)
-login_manager.login_view = 'login'
-
-
-
-
-
-
-
-
-
 
 @app.route('/api/dados', methods=['GET'])
 
 def obter_dados():
     return jsonify({"dados": "Conectado ao Flask com sucesso!"})
-
-@app.route('/api/produtos'), 
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
