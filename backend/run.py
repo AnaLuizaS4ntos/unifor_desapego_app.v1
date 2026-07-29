e88eded8d44012de6c1aea721a0b8dbecd0980f0
Ana Luíza Dos Santos Souza <92101096+AnaLuizaS4ntos@users.noreply.github.com>
2026-07-29 18:18:31 -0300

    .

--- backend/run.py
+++ backend/run.py
@@ -1,30 +1,23 @@
 from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
-import sqlite3 
-import os 
-
 from app.routes.products import products_bp
 from app.routes.auth import auth_bp
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sua_senha@localhost:5432/'
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'chave-secreta-unidesapego2630')
+# Configurações do Banco do Dados (No Render você usará uma variável de ambiente)
+import os
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:senha@localhost:5432/unidesapego')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.register_blueprint(products_bp, url_prefix='/api/products')
-
 app.register_blueprint(auth_bp, url_prefix='/api/auth')
 
 @app.route('/api/dados', methods=['GET'])
-
 def obter_dados():
     return jsonify({"dados": "Conectado ao Flask com sucesso!"})
 
+# IMPORTANTE: Deixe o app.run() apenas para o seu computador local
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
-    
-
